1db7cbfe43f68db2e54b6a3bdde05d76e824f837
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-07-20 13:36:58 -0500

    Initial commit

---
+++ quiz_game.py
@@ -0,0 +1,9 @@
+print(" Welcome to my quiz!")
+
+playing = input(" Would you like to play? (yes/no) ").lower()
+
+if playing != "yes":
+    print("Exiting game...")
+    quit()
+
+print("Let play :)")
