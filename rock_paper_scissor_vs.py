cf0d8b573d7e71eac884cfead1f51cdd52ca9a71
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-07-21 20:08:18 -0500

    Rock, paper, scissors
    Made Rock, paper, scissors mini project

---
+++ rock_paper_scissor_vs.py
@@ -0,0 +1,46 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+draws = 0
+
+options = ["rock", "paper", "scissors"]  # list, index starts at 0
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower() # lowers all inputs
+    if user_input == "q":
+        break # breaks out of while loop
+
+    if user_input not in options:  # checking if user_input is in this list
+        print("Invalid input, please try again.")
+        continue  # asks for input again
+
+    random_number = random.randint(0, 2)  # randint start on 0 because of python indexing
+    # rock:0, paper:1, scissors:2
+    computer_pick = options[random_number] # computer picks index from options based on random number
+    print("Computer picked", computer_pick + ".")  # commas add space between variables automatically
+
+    if user_input == "rock" and computer_pick == "scissors": # logics
+        print("You win!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You win!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You win!")
+        user_wins += 1
+
+    elif user_input == computer_pick:
+        print("Draw, try again.!")
+        draws += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("You drew", draws, "times.")
+print("Goodbye!")
