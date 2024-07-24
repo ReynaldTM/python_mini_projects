0448b3a6f5ca1dbae653b93e07ff25bb4b2867ee
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-07-23 21:53:20 -0500

    Will work on master password later

--- password_manager.py
+++ password_manager.py
@@ -1,28 +1,49 @@
-master_pwd = input(" What is the master password? ")
+from cryptography.fernet import Fernet
+
+
+def load_key():  # remember to define functions before using
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''  # multi line commenting- to make sure the function isn't used again, unnecessarily
+
+#master_pwd = input(" What is the master password? ") # didn't work, don't want to do it right now
+key = load_key()
+fer = Fernet(key)
 
 
 def view():
     with open("password.txt", "r") as f:
         for line in f.readlines():  # "readline" and "readlines" are different, apparently
-            data =  line.rstrip() # "rstrip()" remove tailing space
-            user, passw = data.split("|") # syntax that allows less line for making variable
+            data = line.rstrip()  # "rstrip()" remove tailing space
+            user, passw = data.split("|")  # syntax that allows less line for making variable
             # demo
-            # user,passw = [hello", "world"] # will assign input to variables based on index position
-            print(f"User: {user} | Password: {passw}")
+            # user,passw = ["hello", "world"] # will assign input to variables based on index position
+            print("User: ", user, "| Password:", fer.decrypt(passw.encode()).decode())
+
 
 def add():
-    name = input("Account  Name:")
+    name = input("Account  Name: ")
     pwd = input("Password: ")
 
     with open("password.txt", "a") as f:
-        f.write(name + "|" + pwd + "\n")  # will create name and password then advance to next line
+        f.write(name + "|" + fer.encrypt(
+            pwd.encode()).decode() + "\n")  # will create name and password then advance to next line
+        # turing into bytes then encrypt
 
 
 # "with open"- built in python command that can read("r"), write("w"), and append("a") to files
 # will automatically handle closing or any other file interacts safely
-# "w" create file or overide file
+# "w" create file or override file
 # "r" read files
-# "a" add something to end of a file and/or create a file if it doesn't exsist
+# "a" add something to end of a file and/or create a file if it doesn't exist
 
 while True:
     mode = input(" Would you like to add new password, view existing ones, or quit? (view, add)").lower()
